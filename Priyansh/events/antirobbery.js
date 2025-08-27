2afa7760b6216c78ad2b50167fff2bcb1cf09a6e
herogame362 <herogame3626@gmail.com>
2025-08-27 19:11:24 +0600

    Update antirobbery.js

--- Priyansh/events/antirobbery.js
+++ Priyansh/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "Ashikur Rahman",
     description: "Prevent admin changes",
 };
 
@@ -20,7 +20,7 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
                 api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback)
                 api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, false)
                 function editAdminsCallback(err) {
-                  if (err) return api.sendMessage("Che!! stupid. 😝", event.threadID, event.messageID);
+                  if (err) return api.sendMessage("Chi!! stupid. 😝", event.threadID, event.messageID);
                     return api.sendMessage(`» Activate anti-robbery box 🖤 mode`, event.threadID, event.messageID);
                 }
               }
@@ -32,7 +32,7 @@ module.exports.run = async function ({ event, api, Threads, Users }) {
                 api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback)
                 api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, true)
                 function editAdminsCallback(err) {
-                if (err) return api.sendMessage("Che!! Stupid 😝", event.threadID, event.messageID);
+                if (err) return api.sendMessage("Chi!! Stupid 😝", event.threadID, event.messageID);
                 return api.sendMessage(`» Activate anti-robbery box 🖤 mode`, event.threadID, event.messageID);
               }
             }
