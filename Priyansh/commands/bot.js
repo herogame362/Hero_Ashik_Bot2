b379c57813178a71024ab5e5af55cf090c26402f
herogame362 <herogame3626@gmail.com>
2025-08-27 19:58:16 +0600

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.2",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Ashikur Rahman",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
